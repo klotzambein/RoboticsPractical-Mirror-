d9d2ffd2f98c1111d98a8238fc4e08df31622819
Marc <marcgroefsema@gmail.com>
2020-02-17 12:30:59 +0100

    calibrator with tweety example. TODO: add working cv_bridge and topic name for camera feed

---
+++ line_tracker/scripts/line_tracker.py
@@ -0,0 +1,7 @@
+import rospy  ## The ROS library for python
+import cv2    ## The OpenCV library for python
+import numpy as np  ## Numpy 
+from utils import preprocess_image ## Import the preprocess function as implemented in utils.py
+from geometry_msgs.msg import Twist
+
+## Write your line tracker here:
---
+++ line_tracker/scripts/utils.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+    
+## This function receives the image as obtained from the camera topic.
+## Do your image preprocessing here, such that both your line-tracker and the hsv-calibrator tool can use the same preprocessing function
+def preprocess_image(image):
+
+    return image  ## Return the preprocessed image here
