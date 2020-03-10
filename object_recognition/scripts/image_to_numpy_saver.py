15b22457f3c045bac1ae23ba801a535c0e004206
Robin Kock <r.kock@student.rug.nl>
2020-03-10 12:06:31 +0100

    a

--- object_recognition/scripts/image_to_numpy_saver.py
+++ object_recognition/scripts/image_to_numpy_saver.py
@@ -10,6 +10,9 @@ class DataSaver(object):
 
     def __init__(self, file_name):
         self.image_converter = ImageConverter()
+        self.data = []
+        self.file_name = file_name
+        rospy.on_shutdown(self.save)
         self.image = None
         self.image_subscriber = rospy.Subscriber("/camera/image", Image, self.image_callback, queue_size = 1)
         self.cmd_vel_subscriber = rospy.Subscriber("/cmd_vel", Twist, self.cmd_vel_callback, queue_size = 1)
@@ -22,6 +25,12 @@ class DataSaver(object):
         if cmd_vel_msg.linear.x !=0 or cmd_vel_msg.angular.z != 0:
           self.data.append(self.image)
           print("Nr of images: {0}".format(len(self.data)))
+
+    def save(self): 
+        self.data = np.asarray(self.data)
+        file_path = os.environ["HOME"] + "/data/{}.npy".format(self.file_name)
+        np.save(file_path, self.data)
+        print("Numpy array saved to {0}".format(file_path))
 
 if __name__ == "__main__":
     rospy.init_node("image_saver")
