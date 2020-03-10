5877576ec82360ce7b8dbf2e37b34fa4c7ce54be
Robin Kock <r.kock@student.rug.nl>
2020-03-10 11:22:26 +0100

    a

--- object_recognition/scripts/run_network_example.py
+++ object_recognition/scripts/run_network_example.py
@@ -146,6 +146,7 @@ while True:
             msg = rospy.wait_for_message("/camera/image", Image, 0.01)
             has_msg = True
             print("got image")
+    except: 
     finally:
         if has_msg:
             update(msg)
